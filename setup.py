e7ea4edcf56d20937b3cf2a50608344e57f870e9
larebcloud <lareb761@gmail.com>
2026-07-14 23:23:45 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name = "mlprojects",
+    version = '0.0.1',
+    author = "Lareb",
+    author_email = "lareb761@gamil.com",
+    packages = find_packages(),
+    insatll_requires = ['pandas','numpy','seaborn']
+)
