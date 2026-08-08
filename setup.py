6769e67149179478e230ea482b5330adc4e1dc7e
larebcloud <lareb761@gmail.com>
2026-08-08 16:08:35 +0530

    Add MLOps project

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-from setuptools import find_packages,setup
-from typing import list
+from setuptools import find_packages, setup
+from typing import List
 
-HYPEN_E_DOT = '-e.y'
-def get_reqirememts(file_path:str) -> List[str]:
-    '''
-    this function will return a list of requirements 
-    '''
+HYPEN_E_DOT = "-e ."
 
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+
     with open(file_path) as file_obj:
-      requirements=file_obj.readlines()
-      requirements=[req.replace("\n"," ") for req in requirements]
-      if HYPEN_E_DOT in requirements:
-       requirements.remove(HYPEN_E_DOT) 
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
     return requirements
 
-setup(
-    name = "mlprojects",
-    version = '0.0.1',
-    author = "Lareb",
-    author_email = "lareb761@gamil.com",
-    packages = find_packages(),
-    insatll_requires = get_requirements(requirements.txt)
 
+setup(
+    name="mlprojects",
+    version="0.0.1",
+    author="Lareb",
+    author_email="lareb761@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
 )
--- src/exception.py
+++ src/exception.py
@@ -19,11 +19,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-   
-   try:
-       a=1/10
-   except Exception as e:
-       logging.info("Divide by zero")
-       raise CustomException
- 
